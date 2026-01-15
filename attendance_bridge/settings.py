5641fdce56669e37d4106eab3e2f25614d200041
chrismotari <chrismotari@gmail.com>
2026-01-15 23:45:35 +0300

    Use postgres db

--- attendance_bridge/settings.py
+++ attendance_bridge/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'attendance_bridge.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': f'django.db.backends.{os.getenv("DB_ENGINE", "sqlite3")}',
+        'NAME': os.getenv('DB_NAME', str(BASE_DIR / 'db.sqlite3')),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
 
